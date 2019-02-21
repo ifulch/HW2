92ba8bfc30a1bfd13bc0329fd31258c2079e381e
ifulch <fulch@tamu.edu>
2019-02-21 23:22:15 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,16 +11,32 @@ class MoviesController < ApplicationController
   end
 
   def index
-    sort_by = params[:sort_by]
+  
+    sort_by = params[:sort_by] || session[:sort_by]
+    session[:sort_by] = sort_by
+    
     if sort_by == "title"
       @title_header = "hilite"
     end 
     if sort_by == "release_date"
       @release_date_header = "hilite"
     end 
+    
     @all_ratings = Movie.ratings
-    @ratings = @all_ratings
-    @ratings = params[:ratings].keys if params.keys.include? "ratings"
+    
+    if params.keys.include? "ratings"
+      @ratings = params[:ratings].keys if params[:ratings].is_a? Hash
+      @ratings = params[:ratings] if params[:ratings].is_a? Array
+    elsif session.keys.include? "ratings"
+      @ratings = session[:ratings]
+    else
+      @ratings = @all_ratings
+    end 
+    
+    session[:ratings] = @ratings
+    if ! ((params.keys.include? 'sort_by') || (params.keys.include? 'ratings'))
+    redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings])
+    end 
     @movies = Movie.where(:rating => @ratings).order(sort_by)
   end
 
